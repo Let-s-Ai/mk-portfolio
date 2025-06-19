7439539f2b642566ec9d51cf1d402dc69abaf4b5
kshadow <kshadow@example.com>
2025-06-19 18:12:14 +0400

    about and resume pages are almost done

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-from flask import Flask, render_template, send_from_directory
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Ensure the static/images directory exists
-os.makedirs(os.path.join(app.static_folder, 'images'), exist_ok=True)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('home.html')
+
+
 @app.route('/resume')
 def resume():
     return render_template('resume.html')
 
-# Serve static files from the static directory
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
